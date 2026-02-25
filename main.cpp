d5594d7937ca12d294a545f39e1e52c6000880d7
yooseungyeon31 <ysy8317433@gmail.com>
2026-02-25 09:11:47 +0900

    정돈

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ int main()
 	int Number1= 0;
 	int Number2 = 0;
 
-	cout << "숫자 2개 입력" << endl;
+	cout << "숫자 2개 입력: " << endl;
 	cin>> Number1 >> Number2;
 
-	cout << Number1 + Number2 << endl;
-	cout << Number1 * Number2 << endl;
-	cout << Number1 / Number2 << endl;
-	cout << Number1 % Number2 << endl;
-	cout << Number1 - Number2 << endl;
+	cout <<"Number1 + Number2 =" << Number1 + Number2 << endl;
+	cout << "Number1 * Number2 =" << Number1 * Number2 << endl;
+	cout << "Number1 / Number2 =" << Number1 / Number2 << endl;
+	cout << "Number1 % Number2 =" << Number1 % Number2 << endl;
+	cout << "Number1 - Number2 =" << Number1 - Number2 << endl;
 
 
 
