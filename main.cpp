4036b2a20dc7c0f64b6f76dd3a837e1b65a6a292
yooseungyeon31 <ysy8317433@gmail.com>
2026-02-25 09:06:42 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int Number1= 0;
+	int Number2 = 0;
+
+	cin >> Number1 >> Number2;
+	cout << "Number1 + Number2" << Number1 + Number2 << endl;
+	cout << Number1 * Number2 << endl;
+	cout << Number1 / Number2 << endl;
+	cout << Number1 % Number2 << endl;
+	cout << Number1 - Number2 << endl;
+
+
+
+
+	return 0;
+}
